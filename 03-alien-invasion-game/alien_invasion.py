9d1614f86e2a3346e8b8cf7ed8832e0fc7293372
yeminko <yeminko47@gmail.com>
2026-01-09 09:06:10 +0630

    Implement Alien Invasion game structure with main loop and event handling

--- 03-alien-invasion-game/alien_invasion.py
+++ 03-alien-invasion-game/alien_invasion.py
@@ -1,2 +1,27 @@
+import pygame
+import sys
+
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1200, 700))
+        pygame.display.set_caption("Alien Invasions")
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()

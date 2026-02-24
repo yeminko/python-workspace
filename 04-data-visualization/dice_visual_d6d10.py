6e84b12b7a061606bb02f6c4cc4e96f127e33242
yeminko <yeminko47@gmail.com>
2026-02-24 19:10:47 +0630

    Add visualization scripts for rolling dice; create separate files for two D6 and D6+D10 rolls

--- 04-data-visualization/dice_visual_d6d10.py
+++ 04-data-visualization/dice_visual_d6d10.py
@@ -1,22 +1,29 @@
 from die import Die
 import plotly.express as px
 
-die = Die()
+die_1 = Die()
+die_2 = Die(10)
 
 results = []
 
-for roll_num in range(1000):
-    result = die.roll()
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 frequencies = []
 
-poss_results = range(1, die.num_sides + 1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(1, max_result + 1)
+
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
-title = "Results of Rolling One D6 1,000 Times"
+title = "Results of Rolling Two D6 and D10 50,000 Times"
 labels = {'x': 'Result', 'y': 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+fig.update_layout(xaxis_dtick=1)
+
 fig.show()
+# fig.write_html('dice_visual_d6d10.html')

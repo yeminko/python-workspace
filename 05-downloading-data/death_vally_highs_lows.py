a3463b8161625e2041ed76c6b1dca884d1bd04d4
yeminko <yeminko47@gmail.com>
2026-02-26 19:13:51 +0630

    Enhance Sitka Weather Data Visualization
    - Updated the data source to 'sitka_weather_2021_simple.csv'.
    - Added functionality to plot both high and low temperatures.
    - Implemented date parsing for accurate x-axis representation.
    - Improved plot aesthetics using seaborn style and added fill between high and low temperatures.

--- 05-downloading-data/death_vally_highs_lows.py
+++ 05-downloading-data/death_vally_highs_lows.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/sitka_weather_2021_simple.csv')
+path = Path('weather_data/death_valley_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
@@ -14,12 +14,16 @@ dates, highs, lows = [], [], []
 
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[4])
-    low = int(row[5])
 
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
@@ -27,7 +31,8 @@ ax.plot(dates, highs, color='red', alpha=0.5)
 ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
